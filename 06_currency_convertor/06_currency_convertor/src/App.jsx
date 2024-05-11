b265cf893624d7601c446d41f64a7669fb577323
Manu Kapoor <manu.web3.0@gmail.com>
2024-05-10 18:36:13 -0600

    <InputBox>

--- 06_currency_convertor/06_currency_convertor/src/App.jsx
+++ 06_currency_convertor/06_currency_convertor/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import InputBox from './components/InputBox'
 
 function App() {
   return (
     <>
       <h1 className="text-4xl bg-orange-500">Currency App</h1>
+      <br />
+      <InputBox />
     </>
   )
 }
--- 06_currency_convertor/06_currency_convertor/src/components/InputBox.jsx
+++ 06_currency_convertor/06_currency_convertor/src/components/InputBox.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
-const InputBox = () => {
-  return <div>InputBox</div>
+function InputBox({ label, amount, className = '' }) {
+  return (
+    <div
+      className={`border-black border-2 bg-white p-3 rounded-lg text-sm flex ${className}`}
+    >
+      {/* fix child 1 below for inline*/}
+      <div className="w-1/2 border-blue-600 border-2">
+        <label className="text-black/40 mb-1 inline-block">label</label>
+        <input
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+        />
+      </div>
+      {/* fix child 2 below for inline*/}
+      <div className="border-green-500 border-2 w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none">
+          <option value="usd">usd</option>
+        </select>
+      </div>
+    </div>
+  )
 }
 
 export default InputBox
