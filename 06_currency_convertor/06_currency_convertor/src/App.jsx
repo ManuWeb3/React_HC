871f05ca4a7e4e10926a694ba58f6e2a8ca01969
Manu Kapoor <manu.web3.0@gmail.com>
2024-05-14 21:04:49 -0600

    InputBox.jsx, index.js, App.jsx
    completed all props of InputBox.jsx + created index.js + added complete UI to App.jsx and editing it

--- 06_currency_convertor/06_currency_convertor/src/App.jsx
+++ 06_currency_convertor/06_currency_convertor/src/App.jsx
@@ -1,14 +1,101 @@
 import { useState } from 'react'
 import './App.css'
-import InputBox from './components/InputBox'
+// import InputBox from './components/InputBox'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
+  // get/set all STATES
+  //1. From currency
+  const [from, setFrom] = useState('usd')
+  //2. To currency
+  const [to, setTo] = useState('inr')
+  //3. From Amount
+  const [amount, setAmount] = useState(0)
+  //4. ConvertedTo amount
+  const [convertedTo, setConvertedTo] = useState(0)
+  //5. useCurrencyInfo hook
+  const currencyInfo = useCurrencyInfo(from) // currencyInfo contains entire API-response object's data {} against the key "usd"
+  const options = Object.keys(currencyInfo) // exclusive keys of currencyInfo{}
+
+  // Swap() functionality = currencies swap
+  const swap = () => {
+    //1. Swap currencyOptions
+    setFrom(to)
+    setTo(from)
+    //2. Swap amounts
+    setConvertedTo(amount)
+    setAmount(convertedTo)
+  }
+
+  // setConvertedTo = display amount in INR
+  // amount = USD-amount
+  // currencyInfo[to] = currencyInfo["inr"] = number
+  const convert = () => {
+    setConvertedTo(amount * currencyInfo[to])
+  }
+
   return (
-    <>
-      <h1 className="text-4xl bg-orange-500">Currency App</h1>
-      <br />
-      <InputBox />
-    </>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          {/* HTML Form that includes all 4 elements below */}
+          <form
+            onSubmit={(e) => {
+              e.preventDefault() // No need to send the input values of form elements to any "action" url
+              convert() // instead, call convert()
+            }}
+          >
+            {/* Top input box - "From" */}
+            <div className="w-full mb-1">
+              <InputBox
+                label="From"
+                amount={amount}
+                // onAmountChange={}
+                currencyOptions={options}
+                // const onCurrencyChange = Fn. Ref. Var. = (arg.) => {body}
+                // currency arg below = "e.target.value" of <InputBox>
+                onCurrencyChange={(currency) => setAmount(amount)}
+                selectCurrency={from}
+              />
+            </div>
+            {/* swap button */}
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
+              >
+                swap
+              </button>
+            </div>
+            {/* Bottom input box - "To" */}
+            <div className="w-full mt-1 mb-4">
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={from}
+                amountDisable
+              />
+            </div>
+            {/* Convert Button */}
+            <button
+              type="submit" // relates to submitting all values of the form at its submisison (onSubmit)
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert {from.toUpperCase()} to {to.toUpperCase()}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- 06_currency_convertor/06_currency_convertor/src/components/InputBox.jsx
+++ 06_currency_convertor/06_currency_convertor/src/components/InputBox.jsx
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useId } from 'react'
 
-function InputBox({ label, amount, className = '' }) {
+// like Remitly
+function InputBox({
+  label,
+  amount,
+  onAmountChange, // state change -> fires useEffect(fetch(API called)) -> data updated in the target currency UI field
+  onCurrencyChange,
+  currencyOptions = [], // "options" that contains Object.keys(currency) in App.jsx
+  selectCurrency = 'inr', // Not = "usd1"
+  amountDisable = false, // needed for Production-grade app
+  currencyDisable = false,
+  className = '',
+}) {
+  const amountInputId = useId()
   return (
     <div
       className={`border-black border-2 bg-white p-3 rounded-lg text-sm flex ${className}`}
     >
       {/* fix child 1 below for inline*/}
       <div className="w-1/2 border-blue-600 border-2">
-        <label className="text-black/40 mb-1 inline-block">label</label>
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-1 inline-block"
+        >
+          {label}
+        </label>
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
+          disabled={amountDisable} // A Boolean attribute which, IF PRESENT, indicates that the user should not be able to interact with the input.
+          value={amount}
+          onChange={(e) => onAmountChange && onAmountChange(e.target.value)} // Exactly wat I saw at Remitly's DYNAMIC conversion usd->inr on their UI
         />
       </div>
       {/* fix child 2 below for inline*/}
-      <div className="border-green-500 border-2 w-1/2 flex flex-wrap justify-end text-right">
+      <div className="border-green-500 border-2 w-1/2 flex flex-wrap justify-end text-right ml-1">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none">
-          <option value="usd">usd</option>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        >
+          {currencyOptions.map((currency) => (
+            // "value" of <option> is submitted to the server at form-submission -> prevented by us for now
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
         </select>
       </div>
     </div>
